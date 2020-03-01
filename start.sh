6f1c1469baf7e194f037a738ce3aa91c6e05c21f
guineapig28 <57983276+guineapig28@users.noreply.github.com>
2020-03-01 10:04:00 +0000

    fixed file name

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scp -f ~/src/python/supercomputer/task otto@192.168.1.68:~/src/python/supercomputer/task
+scp -f ~/src/python/supercomputer-pi/task otto@192.168.1.68:~/src/python/supercomputer-pi/task
